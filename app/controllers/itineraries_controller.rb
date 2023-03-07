40ce8b2a76675a5b931c8e332abcb69bb8809f41
Nnamdi Dibigbo <nnamdi.dibigbo@gmail.com>
2023-03-07 15:32:58 +0000

    added some functionality to the edit method in the Itinerary

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -3,17 +3,22 @@ class ItinerariesController < ApplicationController
   end
 
   def index
+    @itineraries = Itinerary.all
   end
 
   def create
   end
 
   def new
+    @itinerary = Itinerary.new
   end
 
   def edit
+    @itinerary = Itinerary.find(params[:id])
+    @itinerary.public = true
   end
 
   def update
+    
   end
 end
