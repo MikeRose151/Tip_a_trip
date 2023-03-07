17b74c5846077cc84d96025059d70ba77a666d62
Mike Rose <michaelrose151@gmail.com>
2023-03-07 17:07:06 +0000

    added a favourites join table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_07_161943) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_07_165306) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_07_161943) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "favourites", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "activity_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["activity_id"], name: "index_favourites_on_activity_id"
+    t.index ["user_id"], name: "index_favourites_on_user_id"
   end
 
   create_table "itineraries", force: :cascade do |t|
@@ -61,6 +70,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_07_161943) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "favourites", "activities"
+  add_foreign_key "favourites", "users"
   add_foreign_key "itineraries", "users"
   add_foreign_key "itinerary_activities", "activities"
   add_foreign_key "itinerary_activities", "itineraries"
