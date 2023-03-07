c9c7e8a6fdd6b757bc270d97c46b41c6016e4d55
Mike Rose <michaelrose151@gmail.com>
2023-03-07 16:09:27 +0000

    started adding to the activities and itineraries controller but need to create a couple tables so just pushing for now

--- app/controllers/itinerary_controller.rb
+++ app/controllers/itinerary_controller.rb
@@ -1,8 +1,12 @@
 class ItineraryController < ApplicationController
   def show
+    @itinerary = Itinerary.find(params[:id])
+    @favourites = @itinerary.favourites # activities that might become part of the itinerary
+    @activities = @itinerary.itinerary_activities # activities that are part of the itinerary
   end
 
   def index
+    @itineraries = Itinerary.all
   end
 
   def create
