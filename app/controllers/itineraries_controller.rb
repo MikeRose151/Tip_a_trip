0ef50ec162e55b3eb443275c15f5da464dc310d2
Mike Rose <michaelrose151@gmail.com>
2023-03-10 13:47:12 +0000

    fixed error

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -61,12 +61,12 @@ class ItinerariesController < ApplicationController
     @all_favourites = Favourite.all
     @favourites = []
 
-    # @all_favourites.each do |favourite|
-    #   if favourite.user == current_user && favourite.activity.destination == @itinerary.destination
-    #     counter += favourite.activity_id.duration
-    #     @favourites << favourite if counter <= 360
-    #   end
-    # end
+    @all_favourites.each do |favourite|
+      if favourite.user == current_user && favourite.activity.destination == @itinerary.destination
+        # counter += favourite.activity_id.duration
+        @favourites << favourite if counter <= 360
+      end
+    end
     @itinerary_activities = @itinerary.activities
 
     @all_itinerary_activities = ItineraryActivity.all
