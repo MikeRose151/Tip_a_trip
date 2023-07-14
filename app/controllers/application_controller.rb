2518318095ce4b3fd84c71a3bd327dc666dc8f69
Mike Rose <michaelrose151@gmail.com>
2023-07-14 14:24:19 -0400

    completed basic sign up process

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   def after_sign_in_path_for(resource)
-    if current_user.username && current_user.avatar
+    if current_user.username && current_user.photo.id
       itineraries_path
     else
       edit_user_path(current_user)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,17 @@ class UsersController < ApplicationController
 
   def update
     @user = current_user
+    @user.username = user_params[:username]
+    if @user.save!
+      redirect_to itineraries_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username)
   end
 end
