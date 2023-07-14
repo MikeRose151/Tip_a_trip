4852d26035be588434b4a7e854bef206d9511124
Mike Rose <michaelrose151@gmail.com>
2023-07-14 09:25:42 -0400

    added private methods for date validation as it wasn't working properly

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -3,8 +3,24 @@ class Itinerary < ApplicationRecord
   belongs_to :destination
   has_many :itinerary_activities
   has_many :activities, through: :itinerary_activities
-  validates :start_date, presence: true, date: { after_or_equal_to: -> { Date.current }, message: "must be no earlier than today" }
-  validates :end_date, presence: true, date: { after_or_equal_to: :start_date, message: "must be no earlier than start date" }
+
+  validate :start_date_must_be_valid
+  validate :end_date_must_be_valid
+
   validates :title, presence: true
   validates :destination, presence: true
+
+  private
+
+  def start_date_must_be_valid
+    return unless start_date.present? && start_date < Date.current
+
+    errors.add(:start_date, "must be no earlier than today")
+  end
+
+  def end_date_must_be_valid
+    return unless end_date.present? && end_date < start_date
+
+    errors.add(:end_date, "must be no earlier than start date")
+  end
 end
