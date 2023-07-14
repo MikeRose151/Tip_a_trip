3d479413c0f0f54b0cbaa05e64620053f1bd4ac9
Mike Rose <michaelrose151@gmail.com>
2023-07-14 11:26:07 -0400

    re-updated itn model, for seeds to work

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -4,23 +4,8 @@ class Itinerary < ApplicationRecord
   has_many :itinerary_activities
   has_many :activities, through: :itinerary_activities
 
-  validate :start_date_must_be_valid
-  validate :end_date_must_be_valid
-
   validates :title, presence: true
   validates :destination, presence: true
-
-  private
-
-  def start_date_must_be_valid
-    if start_date.present? && start_date < Date.current
-      errors.add(:start_date, "must be no earlier than today")
-    end
-  end
-
-  def end_date_must_be_valid
-    if end_date.present? && end_date < start_date
-      errors.add(:end_date, "must be no earlier than start date")
-    end
-  end
+  validates :start_date, presence: true
+  validates :end_date, presence: true
 end
