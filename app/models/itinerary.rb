92f3b9c93ac34ace852838dad5f7b27fc639d888
Mike Rose <michaelrose151@gmail.com>
2023-07-14 09:09:54 -0400

    added validations for start/end date, title, destination

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -3,4 +3,8 @@ class Itinerary < ApplicationRecord
   belongs_to :destination
   has_many :itinerary_activities
   has_many :activities, through: :itinerary_activities
+  validates :start_date, presence: true, date: { after_or_equal_to: -> { Date.current }, message: "must be no earlier than today" }
+  validates :end_date, presence: true, date: { after_or_equal_to: :start_date, message: "must be no earlier than start date" }
+  validates :title, presence: true
+  validates :destination, presence: true
 end
